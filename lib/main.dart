5226e633891e9e2ca5fc10b522e5c6abfc20bd20
lbtung <batung271@gmail.com>
2024-08-06 09:14:20 +0700

    List of Data

--- lib/main.dart
+++ lib/main.dart
@@ -1,179 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
-
-class Home extends StatefulWidget {
-  const Home({super.key});
+  home: QuoteList(),
+));
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  State<QuoteList> createState() => _QuoteListState();
 }
 
-class _HomeState extends State<Home> {
+class _QuoteListState extends State<QuoteList> {
 
-  int age = 18;
+  List<String> quotes = [
+    'Imagination and fiction make up more than three quarters of our real life.',
+    'A hurtful act is the transference to others of the degradation which we bear in ourselves.',
+    'It usually takes me more than three weeks to prepare a good impromptu speech.'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[800],
+      backgroundColor: Colors.grey[400],
       appBar: AppBar(
         title: Text(
-          'Campus ID Card',
-          style: TextStyle(
-            color: Colors.white,
-          ),
+          'Awesome Quotes',
         ),
+        backgroundColor: Colors.redAccent,
         centerTitle: true,
-        backgroundColor: Colors.grey[700],
-        elevation: 0.0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            age +=1;
-          });
-        },
-        child: Icon(
-          Icons.add
-        ),
-        backgroundColor: Colors.grey[500],
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/neck_blast635.jpg'),
-                radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[300],
-            ),
-            Text(
-              'Name',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 16.0,
-              ),
-            ),
-            SizedBox(height: 1.0),
-            Text(
-              'Ba Tung Le',
-              style: TextStyle(
-                  color: Colors.amber,
-                  letterSpacing: 1.5,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 1.0),
-            Text(
-              'Age',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 16.0,
-              ),
-            ),
-            SizedBox(height: 1.0),
-            Text(
-              '$age',
-              style: TextStyle(
-                  color: Colors.amber,
-                  letterSpacing: 1.5,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'University',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 16.0,
-              ),
-            ),
-            SizedBox(height: 1.0),
-            Text(
-              'College of Information and Communication Technology,',
-              style: TextStyle(
-                  color: Colors.amber,
-                  letterSpacing: 1.5,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'Can Tho University',
-              style: TextStyle(
-                  color: Colors.amber,
-                  letterSpacing: 1.5,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.mail,
-                  color: Colors.grey[400],
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
-                  child: Text(
-                    'lbtung9999@batung.le',
-                    style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 16.0,
-                    ),
-                  ),
-                )
-              ],
-            ),
-            SizedBox(height: 10.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.phone,
-                  color: Colors.grey[400],
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
-                  child: Text(
-                    '0987654321',
-                    style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 16.0,
-                    ),
-                  ),
-                )
-              ],
-            ),
-            SizedBox(height: 10.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.house,
-                  color: Colors.grey[400],
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
-                  child: Text(
-                    'Can Tho City',
-                    style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 16.0,
-                    ),
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
+      body: Column(
+        children: quotes.map((quote){
+          return Text(quote);
+        }).toList(),
       ),
     );
   }
 }
+
